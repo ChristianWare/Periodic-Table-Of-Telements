a1fa5e97fd405d65f23d979d19da0ce4c9d3f39b
Chris <chris.ware.dev@gmail.com>
2021-11-23 16:58:01 -0700

    Row 3 Configuration

---
+++ src/components/Row3.js
@@ -0,0 +1,48 @@
+import styles from "../../styles/Row3.module.css";
+
+function Row3() {
+  return (
+    <div className={styles.row2Container}>
+      <div className={styles.content}>
+        <div className={styles.box1}>
+          <div className={styles.elementDetails}>
+            <h2 className={styles.elementSymbol}>Na</h2>
+            <p className={styles.elementname}>Lithium</p>
+          </div>
+          <div className={styles.elementDetails}>
+            <h2 className={styles.elementSymbol}>Mg</h2>
+            <p className={styles.elementname}>Beronium</p>
+          </div>
+        </div>
+        <div className={styles.box2}>
+          <div className={styles.elementDetails}>
+            <h2 className={styles.elementSymbol}>Ai</h2>
+            <p className={styles.elementname}>Helium</p>
+          </div>
+          <div className={styles.elementDetails}>
+            <h2 className={styles.elementSymbol}>Si</h2>
+            <p className={styles.elementname}>Helium</p>
+          </div>
+          <div className={styles.elementDetails}>
+            <h2 className={styles.elementSymbol}>P</h2>
+            <p className={styles.elementname}>Helium</p>
+          </div>
+          <div className={styles.elementDetails}>
+            <h2 className={styles.elementSymbol}>S</h2>
+            <p className={styles.elementname}>Helium</p>
+          </div>
+          <div className={styles.elementDetails}>
+            <h2 className={styles.elementSymbol}>Ci</h2>
+            <p className={styles.elementname}>Helium</p>
+          </div>
+          <div className={styles.elementDetails}>
+            <h2 className={styles.elementSymbol}>Ar</h2>
+            <p className={styles.elementname}>Helium</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Row3;
