3e2a326a877a5c042acaf80ca7eda1581846b4ac
Chris <chris.ware.dev@gmail.com>
2021-11-23 19:42:21 -0700

    Row's 4567

--- src/components/Row4567.js
+++ src/components/Row4567.js
@@ -1,4 +1,4 @@
-import styles from "../../styles/Row45.module.css";
+import styles from "../../styles/Row4567.module.css";
 
 const elements = [
   {
@@ -181,9 +181,189 @@ const elements = [
     symbol: "Pd",
     id: 18,
   },
+  {
+    name: "Nickel",
+    symbol: "Nl",
+    id: 1,
+  },
+  {
+    name: "Copper",
+    symbol: "Co",
+    id: 2,
+  },
+  {
+    name: "Zinc",
+    symbol: "Zn",
+    id: 3,
+  },
+  {
+    name: "Gallium",
+    symbol: "G",
+    id: 4,
+  },
+  {
+    name: "Germanium",
+    symbol: "Gm",
+    id: 5,
+  },
+  {
+    name: "Arsenic",
+    symbol: "Ar",
+    id: 6,
+  },
+  {
+    name: "Selenium",
+    symbol: "NL",
+    id: 7,
+  },
+  {
+    name: "Bromine",
+    symbol: "Be",
+    id: 8,
+  },
+  {
+    name: "Krypton",
+    symbol: "Kt",
+    id: 9,
+  },
+  {
+    name: "Rubidium",
+    symbol: "Ru",
+    id: 10,
+  },
+  {
+    name: "Strontium",
+    symbol: "NL",
+    id: 11,
+  },
+  {
+    name: "Yttrium",
+    symbol: "NL",
+    id: 12,
+  },
+  {
+    name: "Zirconium",
+    symbol: "Sn",
+    id: 13,
+  },
+  {
+    name: "Niobium",
+    symbol: "Js",
+    id: 14,
+  },
+  {
+    name: "Molybdenum",
+    symbol: "Mo",
+    id: 15,
+  },
+  {
+    name: "Technetium",
+    symbol: "Tc",
+    id: 16,
+  },
+  {
+    name: "Rhodium",
+    symbol: "Rh",
+    id: 17,
+  },
+  {
+    name: "Palladium",
+    symbol: "Pd",
+    id: 18,
+  },
+  {
+    name: "Nickel",
+    symbol: "Nl",
+    id: 19,
+  },
+  {
+    name: "Copper",
+    symbol: "Co",
+    id: 20,
+  },
+  {
+    name: "Zinc",
+    symbol: "Zn",
+    id: 30,
+  },
+  {
+    name: "Gallium",
+    symbol: "G",
+    id: 40,
+  },
+  {
+    name: "Germanium",
+    symbol: "Gm",
+    id: 50,
+  },
+  {
+    name: "Arsenic",
+    symbol: "Ar",
+    id: 60,
+  },
+  {
+    name: "Selenium",
+    symbol: "NL",
+    id: 70,
+  },
+  {
+    name: "Bromine",
+    symbol: "Be",
+    id: 80,
+  },
+  {
+    name: "Krypton",
+    symbol: "Kt",
+    id: 90,
+  },
+  {
+    name: "Rubidium",
+    symbol: "Ru",
+    id: 100,
+  },
+  {
+    name: "Strontium",
+    symbol: "NL",
+    id: 110,
+  },
+  {
+    name: "Yttrium",
+    symbol: "NL",
+    id: 120,
+  },
+  {
+    name: "Zirconium",
+    symbol: "Sn",
+    id: 13,
+  },
+  {
+    name: "Niobium",
+    symbol: "Js",
+    id: 14,
+  },
+  {
+    name: "Molybdenum",
+    symbol: "Mo",
+    id: 15,
+  },
+  {
+    name: "Technetium",
+    symbol: "Tc",
+    id: 16,
+  },
+  {
+    name: "Rhodium",
+    symbol: "Rh",
+    id: 17,
+  },
+  {
+    name: "Palladium",
+    symbol: "Pd",
+    id: 18,
+  },
 ];
 
-function Row45() {
+function Row4567() {
   return (
     <div className={styles.row4Container}>
       <div className={styles.content}>
@@ -200,4 +380,4 @@ function Row45() {
   );
 }
 
-export default Row45;
+export default Row4567;
