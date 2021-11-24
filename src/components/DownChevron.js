a8baa48c82abda2c9e69e3dd1945cd67519c2380
Chris <chris.ware.dev@gmail.com>
2021-11-24 11:58:22 -0700

    64px Box

---
+++ src/components/DownChevron.js
@@ -0,0 +1,11 @@
+import styles from '../../styles/DownChevron.module.css'
+
+function DownChevron() {
+    return (
+        <div className={styles.arrowContainer}>
+            
+        </div>
+    )
+}
+
+export default DownChevron
