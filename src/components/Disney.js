f801def3afd27efcbdb5d4502b4a0a525da51ae7
Chris <chris.ware.dev@gmail.com>
2021-11-26 00:23:39 -0700

    Disney/Paramount Sections

--- src/components/Disney.js
+++ src/components/Disney.js
@@ -1,32 +1,38 @@
 import styles from "../../styles/Disney.module.css";
+import { RiArrowRightDownLine } from "react-icons/ri";
 
 function Disney({ disney }) {
   return (
-    <div className={styles.disneyContainer}>
-      <div className={styles.text}>
-        <h1>
-          DISNEY <br /> <span className={styles.tel}>TELE</span>MENTS
-        </h1>
-      </div>
-      <div className={styles.content}>
-        {disney.results.map((d) => (
-          <div key={d.title} className={styles.box1}>
-            <div className={styles.elementDetails}>
-              <h2 className={styles.elementSymbol}>{d.title.slice(0, 2)}</h2>
-              <p className={styles.elementname}>{d.title}</p>
+    <>
+      <div className={styles.disneyContainer}>
+        <div className={styles.text}>
+          <h1>
+            DISNEY <br /> <span className={styles.tel}>TELE</span>MENTS
+          </h1>
+        </div>
+        <div className={styles.content}>
+          {disney.results.map((d) => (
+            <div key={d.title} className={styles.box1}>
+              <div className={styles.elementDetails}>
+                <h2 className={styles.elementSymbol}>{d.title.slice(0, 2)}</h2>
+                <p className={styles.elementname}>{d.title}</p>
+              </div>
             </div>
-          </div>
-        ))}
-        {disney.results.map((d) => (
-          <div key={d.title} className={styles.box1}>
-            <div className={styles.elementDetails}>
-              <h2 className={styles.elementSymbol}>{d.title.slice(4, 6)}</h2>
-              <p className={styles.elementname}>{d.title}</p>
+          ))}
+          {disney.results.map((d) => (
+            <div key={d.title} className={styles.box1}>
+              <div className={styles.elementDetails}>
+                <h2 className={styles.elementSymbol}>{d.title.slice(4, 6)}</h2>
+                <p className={styles.elementname}>{d.title}</p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+      <div className={styles.arrowContainer}>
+        <RiArrowRightDownLine className={styles.arrow} />
+      </div>
+    </>
   );
 }
 
--- src/components/Paramount.js
+++ src/components/Paramount.js
@@ -1,32 +1,38 @@
 import styles from '../../styles/Paramount.module.css'
+import { RiArrowLeftDownLine } from 'react-icons/ri'
 
 function Paramount({ para }) {
   return (
-    <div className={styles.disneyContainer}>
-      <div className={styles.content}>
-        {para.results.map((p) => (
-          <div key={p.title} className={styles.box1}>
-            <div className={styles.elementDetails}>
-              <h2 className={styles.elementSymbol}>{p.title.slice(1, 3)}</h2>
-              <p className={styles.elementname}>{p.title}</p>
+    <>
+      <div className={styles.paramountContainer}>
+        <div className={styles.content}>
+          {para.results.map((p) => (
+            <div key={p.title} className={styles.box1}>
+              <div className={styles.elementDetails}>
+                <h2 className={styles.elementSymbol}>{p.title.slice(1, 3)}</h2>
+                <p className={styles.elementname}>{p.title}</p>
+              </div>
             </div>
-          </div>
-        ))}
-        {para.results.map((p) => (
-          <div key={p.title} className={styles.box1}>
-            <div className={styles.elementDetails}>
-              <h2 className={styles.elementSymbol}>{p.title.slice(1, 3)}</h2>
-              <p className={styles.elementname}>{p.title}</p>
+          ))}
+          {para.results.map((p) => (
+            <div key={p.title} className={styles.box1}>
+              <div className={styles.elementDetails}>
+                <h2 className={styles.elementSymbol}>{p.title.slice(1, 3)}</h2>
+                <p className={styles.elementname}>{p.title}</p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
+        <div className={styles.text}>
+          <h1>
+            PARAMOUNT <br /> <span className={styles.tel}>TELE</span>MENTS
+          </h1>
+        </div>
       </div>
-      <div className={styles.text}>
-        <h1>
-          PARAMOUNT <br /> <span className={styles.tel}>TELE</span>MENTS
-        </h1>
+      <div className={styles.arrowContainer}>
+        <RiArrowLeftDownLine className={styles.arrow} />
       </div>
-    </div>
+    </>
   );
 }
 
