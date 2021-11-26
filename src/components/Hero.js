70d56d94f9407cf31a678e11360969f51e76a787
Chris <chris.ware.dev@gmail.com>
2021-11-25 17:43:20 -0700

    Hero Section

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import styles from '../../styles/Hero.module.css'
+
+function Hero() {
+    return (
+      <div className={styles.heroContainer}>
+        <h1>PERIODIC TABLE OF <br /> TELEMENTS</h1>
+      </div>
+    );
+}
+
+
+
+export default Hero
