97384e5eed825d6d960c18cf5cef894f0a27063c
Chris <chris.ware.dev@gmail.com>
2021-11-25 20:42:57 -0700

    Hero Updates

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,7 @@ import styles from '../../styles/Hero.module.css'
 function Hero() {
     return (
       <div className={styles.heroContainer}>
-        <h1>PERIODIC TABLE OF <br /> TELEMENTS</h1>
+        <h1>PERIODIC TABLE OF <br /> <span className={styles.tel}>TELE</span>MENTS</h1>
       </div>
     );
 }
