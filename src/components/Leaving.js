791a9e6a9fc03d3d58905301302f6e0ee125a9d8
Chris <chris.ware.dev@gmail.com>
2021-11-27 21:43:06 -0700

    45deg UI Lines

--- src/components/Leaving.js
+++ src/components/Leaving.js
@@ -13,7 +13,7 @@ function Leaving({ nfLeaving, huluLeaving, primeLeaving, hboLeaving }) {
           <div className={styles.netflixContent}>
             <h2 className={styles.telNetflix}>NETFLIX</h2>
             {nfLeaving.results.slice(0, 10).map((n) => (
-              <p>{n.title.slice(0, 34)}</p>
+              <p>{n.title.slice(0, 30)}</p>
             ))}
           </div>
         </div>
@@ -22,26 +22,29 @@ function Leaving({ nfLeaving, huluLeaving, primeLeaving, hboLeaving }) {
           <div className={styles.hulucontent}>
             <h2 className={styles.telHulu}>HULU</h2>
             {huluLeaving.results.slice(10, 20).map((h) => (
-              <p>{h.title}</p>
+              <p>{h.title.slice(0, 30)}</p>
             ))}
           </div>
         </div>
       </div>
       <div className={styles.leavingList}>
         <div className={styles.leavingListItem}>
-          <h2 className={styles.telPrime}>PRIME</h2>
-          {primeLeaving.results.slice(0, 10).map((p) => (
-            <p>{p.title}</p>
-          ))}
+          <div className={styles.primeContent}>
+            <h2 className={styles.telPrime}>PRIME</h2>
+            {primeLeaving.results.slice(0, 10).map((p) => (
+              <p>{p.title.slice(0, 30)}</p>
+            ))}
+          </div>
         </div>
 
         <div className={styles.leavingListItem}>
           <h2 className={styles.telHBO}>HBO MAX</h2>
           {hboLeaving.results.slice(18, 28).map((hb) => (
-            <p>{hb.title}</p>
+            <p>{hb.title.slice(0, 30)}</p>
           ))}
         </div>
       </div>
+      <div className={styles.lines}></div>
     </div>
   );
 }
